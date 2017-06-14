8a6d3f4ac23b138cbd5b4708c1d97a18a76b5e7c
zeatul@qq.com <zeatul@qq.com>
2017-06-15 01:24:47 +0800

    1

---
+++ e-commerce/e-commerce-mall-product-service/src/main/java/com/hawk/ecom/product/service/SkuService.java
@@ -0,0 +1,16 @@
+package com.hawk.ecom.product.service;
+
+import org.springframework.stereotype.Service;
+
+import com.hawk.ecom.product.persist.domain.SkuDomain;
+import com.hawk.ecom.product.request.CreateSkuParam;
+
+@Service
+public class SkuService {
+	
+	public SkuDomain createSku(CreateSkuParam createSkuParam ){
+		return null;
+		
+	}
+
+}
