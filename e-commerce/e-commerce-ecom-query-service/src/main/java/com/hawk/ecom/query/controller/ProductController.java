64a3976a383038ec661f21ca7477f37cfcc46de7
zeatul@qq.com <zeatul@qq.com>
2017-06-30 13:18:15 +0800

    1

--- e-commerce/e-commerce-ecom-query-service/src/main/java/com/hawk/ecom/query/controller/ProductController.java
+++ e-commerce/e-commerce-ecom-query-service/src/main/java/com/hawk/ecom/query/controller/ProductController.java
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hawk.ecom.pub.response.MultiResponse;
 import com.hawk.ecom.query.persist.domainex.ProductCategoryExDomain;
 import com.hawk.ecom.query.persist.domainex.ProductSkuExDomain;
-import com.hawk.ecom.query.persist.domainex.SystemResourceExDomain;
 import com.hawk.ecom.query.request.ListSkuParam;
 import com.hawk.ecom.query.service.ProductService;
+import com.hawk.framework.pub.sql.PagingQueryResultWrap;
 import com.hawk.framework.pub.web.HttpRequestTools;
 import com.hawk.framework.pub.web.SuccessResponse;
 import com.hawk.framework.pub.web.WebResponse;
@@ -41,16 +41,18 @@ public class ProductController {
 	 * @throws Exception
 	 */
 	@RequestMapping(value = "/category/list", method = {POST,GET})
-	public WebResponse<MultiResponse<ProductCategoryExDomain>> queryCategory(HttpServletRequest request) throws Exception{
-		List<ProductCategoryExDomain> list = productService.queryCategory();
+	public WebResponse<MultiResponse<ProductCategoryExDomain>> listCategory(HttpServletRequest request) throws Exception{
+		List<ProductCategoryExDomain> list = productService.listCategory();
 		return SuccessResponse.build(new MultiResponse<ProductCategoryExDomain>(list));
 	}
 	
 	@RequestMapping(value = "/sku/list", method = {POST,GET})
 	public WebResponse<MultiResponse<ProductSkuExDomain>> querySku(HttpServletRequest request) throws Exception{
-		ListSkuParam param = HttpRequestTools.parse(request, ListSkuParam.class);		
-		List<ProductSkuExDomain> list = productService.querySku(param);
-		return SuccessResponse.build(new MultiResponse<ProductSkuExDomain>(list));
+		ListSkuParam param = HttpRequestTools.parse(request, ListSkuParam.class);
+		PagingQueryResultWrap<ProductSkuExDomain> wrap = productService.querySku(param);
+
+		MultiResponse<ProductSkuExDomain> result = new MultiResponse<ProductSkuExDomain>(wrap);
+		return SuccessResponse.build(result);
 	}
 	
 	@RequestMapping(value = "/sku/load/id/{skuId}", method = {POST,GET})
@@ -58,7 +60,7 @@ public class ProductController {
 		 return SuccessResponse.build(productService.loadSku(skuId));
 	}
 	
-	@RequestMapping(value = "/sku/loadTransdata/id/{skuId}", method = {POST,GET})
+	@RequestMapping(value = "/sku/loadSaleData/id/{skuId}", method = {POST,GET})
 	public WebResponse<ProductSkuExDomain> loadSkuPriceAndQuantity(@PathVariable Integer skuId) throws Exception{
 		return SuccessResponse.build(productService.loadSkuPriceAndQuantity(skuId));
 	}
--- e-commerce/e-commerce-ecom-query-service/src/test/java/com/hawk/ecom/query/controller/SystemResourceControllerTest.java
+++ e-commerce/e-commerce-ecom-query-service/src/test/java/com/hawk/ecom/query/controller/SystemResourceControllerTest.java
@@ -10,11 +10,11 @@ import com.hawk.framework.utility.http.HttpExecutor.HttpParam;
 public class SystemResourceControllerTest extends AbstractControllerTest{
 
 	@Test
-	public void testHome(){
+	public void testH5main(){
 		String url = getUrl("/ecom/query/system/resource/h5main");
 		List<HttpParam> params = new ArrayList<HttpParam>();
 		params.add(new HttpParam("version", "1.0"));
-		String result = httpExecutor.get(url, null);
+		String result = httpExecutor.get(url, params);
 		System.out.println("result="+result);
 	}
 }
