5ddc0e136a0a5a9e566cef37980251a548cfbc91
zeatul@qq.com <zeatul@qq.com>
2017-07-26 17:03:31 +0800

    1

--- e-commerce/e-commerce-mall-product-service/src/main/java/com/hawk/ecom/product/controller/EnvironmentController.java
+++ e-commerce/e-commerce-mall-product-service/src/main/java/com/hawk/ecom/product/controller/EnvironmentController.java
@@ -51,27 +51,27 @@ import com.hawk.framework.utility.tools.StringTools;
 @RequestMapping("/mall/admin/product/env/")
 @CrossOrigin
 public class EnvironmentController {
-	
+
 	@Autowired
 	private CategoryService categoryService;
-	
+
 	@Autowired
 	private AttrNameService attrNameService;
-	
+
 	@Autowired
 	private AttrValueService attrValueService;
-	
+
 	@Autowired
 	private ProductService productService;
-	
+
 	@Autowired
 	private SkuService skuService;
-	
+
 	@Autowired
 	private StockService stockService;
-	
+
 	private String userCode = "000002";
-	
+
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@RequestMapping(value = "/init", method = { POST, GET })
@@ -79,8 +79,8 @@ public class EnvironmentController {
 
 		AuthThreadLocal.setMobileNumber("13300000000");
 		AuthThreadLocal.setStoreCode("ST000001");
-		AuthThreadLocal.setUserCode(userCode); //admin
-		
+		AuthThreadLocal.setUserCode(userCode); // admin
+
 		build();
 
 		return "Succeeded to init the product environment" + ", current time = " + DateTools.convert(new Date(), DateTools.DATETIME_SSS_PATTERN);
@@ -98,35 +98,71 @@ public class EnvironmentController {
 		createCategorParam.setCategoryCode("virtual_product");
 		createCategorParam.setOperatorCode(userCode);
 		createCategorParam.setPid(0);
-		
+
 		CategoryDomain virtualGoodsCategory = categoryService.createCategory(createCategorParam);
-		
+
 		createCategorParam = new CreateCategoryParam();
 		createCategorParam.setCategoryName("碎屏险");
 		createCategorParam.setIsLeaf(1);
 		createCategorParam.setOperatorCode(userCode);
 		createCategorParam.setPid(virtualGoodsCategory.getId());
-		
+
 		CategoryDomain screenBrokenCategory = categoryService.createCategory(createCategorParam);
-		
+
 		createCategorParam = new CreateCategoryParam();
 		createCategorParam.setCategoryName("流量充值");
 		createCategorParam.setCategoryCode("charge_data");
 		createCategorParam.setIsLeaf(1);
 		createCategorParam.setOperatorCode(userCode);
-		createCategorParam.setPid(virtualGoodsCategory.getId());		
+		createCategorParam.setPid(virtualGoodsCategory.getId());
 		CategoryDomain mobileDataChargeCategory = categoryService.createCategory(createCategorParam);
+
+		buildChargeDataProduct(mobileDataChargeCategory);
+		buildBsiProduct(screenBrokenCategory);
 		
+
+		logger.info("Succeeded to generate all products");
+	}
+
+	public void buildBsiProduct(CategoryDomain screenBrokenCategory) {
 		/**
 		 * 更新变式状态为编辑状态，以创建属性名和属性值
 		 */
 		UpdateCategoryVariantStatusParam updateCategoryVariantStatusParam = new UpdateCategoryVariantStatusParam();
 		updateCategoryVariantStatusParam.setOperatorCode(userCode);
 		updateCategoryVariantStatusParam.setCategoryVariantStatus(ConstCategory.CategoryVariantStatus.EDITING);
-		updateCategoryVariantStatusParam.setIds(Arrays.asList(screenBrokenCategory.getId(),mobileDataChargeCategory.getId()));
+		updateCategoryVariantStatusParam.setIds(Arrays.asList(screenBrokenCategory.getId()));
 		categoryService.updateCategoryVariantStatus(updateCategoryVariantStatusParam);
 		
+		/**
+		 * 创建产品分类碎屏险的属性名和属性值，属性名:(保险月数,保险档次)
+		 */
+		CreateAttrNameParam createAttrNameParam = new CreateAttrNameParam();
+		createAttrNameParam.setAttrName("保险月数");
+		createAttrNameParam.setAttrNameCode(ConstAttrNameCode.Bsi.INSURANCE_PERIOD_MONTH);
+		createAttrNameParam.setAttrNameBusinessType(ConstAttr.AttrNameBusinessType.OTHERS);
+		createAttrNameParam.setAttrValueType(ConstAttr.AttrValueType.INTEGER);
+		createAttrNameParam.setCategoryId(screenBrokenCategory.getId());
+		createAttrNameParam.setIsSearch(ConstBoolean.TRUE);
+		createAttrNameParam.setPid(0);
+		createAttrNameParam.setPvid(0);
+		createAttrNameParam.setOperatorCode(userCode);
+		AttrNameDomain insurancePeriodMonthAttrNameDomain = attrNameService.createAttrName(createAttrNameParam);
 		
+		List<AttrValueDomain> insurancePeriodMonthDomainList = new ArrayList<AttrValueDomain>();
+	}
+
+	public void buildChargeDataProduct(CategoryDomain mobileDataChargeCategory) {
+
+		/**
+		 * 更新变式状态为编辑状态，以创建属性名和属性值
+		 */
+		UpdateCategoryVariantStatusParam updateCategoryVariantStatusParam = new UpdateCategoryVariantStatusParam();
+		updateCategoryVariantStatusParam.setOperatorCode(userCode);
+		updateCategoryVariantStatusParam.setCategoryVariantStatus(ConstCategory.CategoryVariantStatus.EDITING);
+		updateCategoryVariantStatusParam.setIds(Arrays.asList(mobileDataChargeCategory.getId()));
+		categoryService.updateCategoryVariantStatus(updateCategoryVariantStatusParam);
+
 		/**
 		 * 创建产品分类流量充值的属性名和属性值 , 属性名：移动服务商(电信,移动,联通)
 		 */
@@ -141,9 +177,9 @@ public class EnvironmentController {
 		createAttrNameParam.setPvid(0);
 		createAttrNameParam.setOperatorCode(userCode);
 		AttrNameDomain mobileSupplierAttrNameDomain = attrNameService.createAttrName(createAttrNameParam);
-		
+
 		List<AttrValueDomain> supplierDomainList = new ArrayList<AttrValueDomain>();
-		
+
 		CreateAttrValueParam createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("电信");
 		createAttrValueParam.setAttrNameId(mobileSupplierAttrNameDomain.getId());
@@ -152,7 +188,7 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("telecom");
 		AttrValueDomain telecomSupplierAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		supplierDomainList.add(telecomSupplierAttrValueDomain);
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("联通");
 		createAttrValueParam.setAttrNameId(mobileSupplierAttrNameDomain.getId());
@@ -161,7 +197,7 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("uniom");
 		AttrValueDomain unicomSupplierAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		supplierDomainList.add(unicomSupplierAttrValueDomain);
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("移动");
 		createAttrValueParam.setAttrNameId(mobileSupplierAttrNameDomain.getId());
@@ -170,9 +206,9 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("mobile");
 		AttrValueDomain mobileSupplierAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		supplierDomainList.add(mobileSupplierAttrValueDomain);
-		
+
 		/**
-		 * 创建产品分类流量充值的属性名和属性值   ,属性名：流量大小(100M,1000M,5000M)
+		 * 创建产品分类流量充值的属性名和属性值 ,属性名：流量大小(100M,1000M,5000M)
 		 */
 		createAttrNameParam = new CreateAttrNameParam();
 		createAttrNameParam.setAttrNameCode(ConstAttrNameCode.Mobile.DATA_SIZE);
@@ -185,9 +221,9 @@ public class EnvironmentController {
 		createAttrNameParam.setPvid(0);
 		createAttrNameParam.setOperatorCode(userCode);
 		AttrNameDomain chargeQuantityAttrNameDomain = attrNameService.createAttrName(createAttrNameParam);
-		
+
 		List<AttrValueDomain> chargeQuantityDomainList = new ArrayList<AttrValueDomain>();
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("100M");
 		createAttrValueParam.setAttrNameId(chargeQuantityAttrNameDomain.getId());
@@ -196,7 +232,7 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("100M");
 		AttrValueDomain charge100MAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		chargeQuantityDomainList.add(charge100MAttrValueDomain);
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("1000M");
 		createAttrValueParam.setAttrNameId(chargeQuantityAttrNameDomain.getId());
@@ -205,7 +241,7 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("1000M");
 		AttrValueDomain charge1000MAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		chargeQuantityDomainList.add(charge1000MAttrValueDomain);
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("5000M");
 		createAttrValueParam.setAttrNameId(chargeQuantityAttrNameDomain.getId());
@@ -214,11 +250,11 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("5000M");
 		AttrValueDomain charge5000MAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		chargeQuantityDomainList.add(charge5000MAttrValueDomain);
-		
+
 		/**
 		 * 创建产品分类流量充值的属性名和属性值 , 属性名:地区(江苏,上海,广东)
 		 */
-		
+
 		createAttrNameParam = new CreateAttrNameParam();
 		createAttrNameParam.setAttrNameCode(ConstAttrNameCode.Mobile.PROVINCE);
 		createAttrNameParam.setAttrName("地区");
@@ -230,9 +266,9 @@ public class EnvironmentController {
 		createAttrNameParam.setPvid(0);
 		createAttrNameParam.setOperatorCode(userCode);
 		AttrNameDomain districtAttrNameDomain = attrNameService.createAttrName(createAttrNameParam);
-		
+
 		List<AttrValueDomain> districtDomainList = new ArrayList<AttrValueDomain>();
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("江苏");
 		createAttrValueParam.setAttrNameId(districtAttrNameDomain.getId());
@@ -241,7 +277,7 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("jiangsu");
 		AttrValueDomain jiangsuDistrictAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		districtDomainList.add(jiangsuDistrictAttrValueDomain);
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("上海");
 		createAttrValueParam.setAttrNameId(districtAttrNameDomain.getId());
@@ -250,7 +286,7 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("shanghai");
 		AttrValueDomain shanghaiDistrictAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		districtDomainList.add(shanghaiDistrictAttrValueDomain);
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("广东");
 		createAttrValueParam.setAttrNameId(districtAttrNameDomain.getId());
@@ -259,7 +295,7 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("guangdong");
 		AttrValueDomain guangdongDistrictAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		districtDomainList.add(guangdongDistrictAttrValueDomain);
-		
+
 		/**
 		 * 创建产品分类流量充值的属性名和属性值 , 属性名：流量类型(本省,省漫)
 		 */
@@ -274,9 +310,9 @@ public class EnvironmentController {
 		createAttrNameParam.setPvid(0);
 		createAttrNameParam.setOperatorCode(userCode);
 		AttrNameDomain mobileDataTypeAttrNameDomain = attrNameService.createAttrName(createAttrNameParam);
-		
+
 		List<AttrValueDomain> mobileDataTypeDomainList = new ArrayList<AttrValueDomain>();
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("本地使用");
 		createAttrValueParam.setAttrNameId(mobileDataTypeAttrNameDomain.getId());
@@ -285,7 +321,7 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("province");
 		AttrValueDomain provinceDataTypeAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		mobileDataTypeDomainList.add(provinceDataTypeAttrValueDomain);
-		
+
 		createAttrValueParam = new CreateAttrValueParam();
 		createAttrValueParam.setAttrDisplayValue("全国通用");
 		createAttrValueParam.setAttrNameId(mobileDataTypeAttrNameDomain.getId());
@@ -294,32 +330,31 @@ public class EnvironmentController {
 		createAttrValueParam.setAttrDisplayEnValue("country");
 		AttrValueDomain countryDataTypeAttrValueDomain = attrValueService.createAttrValue(createAttrValueParam);
 		mobileDataTypeDomainList.add(countryDataTypeAttrValueDomain);
-		
-		
+
 		/**
 		 * 更新变式状态为可用状态，以创建产品
 		 */
 		updateCategoryVariantStatusParam = new UpdateCategoryVariantStatusParam();
 		updateCategoryVariantStatusParam.setOperatorCode(userCode);
 		updateCategoryVariantStatusParam.setCategoryVariantStatus(ConstCategory.CategoryVariantStatus.AVAILABLE);
-		updateCategoryVariantStatusParam.setIds(Arrays.asList(screenBrokenCategory.getId(),mobileDataChargeCategory.getId()));
+		updateCategoryVariantStatusParam.setIds(Arrays.asList(mobileDataChargeCategory.getId()));
 		categoryService.updateCategoryVariantStatus(updateCategoryVariantStatusParam);
-		
+
 		/**
-		 * 创建产品,产品Sku,产品库存
-		 * 运营商，流量大小，流量类型,地区全部为关键属性.
+		 * 创建产品,产品Sku,产品库存 运营商，流量大小，流量类型,地区全部为关键属性.
 		 */
-//		List<ProductDomain> productDomainList = new ArrayList<ProductDomain>();
-		for(AttrValueDomain supplierDomain : supplierDomainList){
-			for(AttrValueDomain chargeQuantityDomain : chargeQuantityDomainList){
-				for(AttrValueDomain districtDomain : districtDomainList){
-					for(AttrValueDomain mobildDataDomain : mobileDataTypeDomainList){
-						
+		// List<ProductDomain> productDomainList = new
+		// ArrayList<ProductDomain>();
+		for (AttrValueDomain supplierDomain : supplierDomainList) {
+			for (AttrValueDomain chargeQuantityDomain : chargeQuantityDomainList) {
+				for (AttrValueDomain districtDomain : districtDomainList) {
+					for (AttrValueDomain mobildDataDomain : mobileDataTypeDomainList) {
+
 						/**
 						 * 产品
 						 */
 						CreateProductParam createProductParam = new CreateProductParam();
-						
+
 						createProductParam.setCategoryId(mobileDataChargeCategory.getId());
 						createProductParam.setIsVirtual(ConstBoolean.TRUE);
 						createProductParam.setDeliveryType(ConstProduct.DeliveryType.CHARGE_FLOW_DATA);
@@ -327,25 +362,22 @@ public class EnvironmentController {
 						createProductParam.setProductCode(null);
 						createProductParam.setProductDesc(null);
 						createProductParam.setProductHomePage("http://www.baidu.com");
-						List<Integer> productKeyAttrValueIds = Arrays.asList(supplierDomain.getId(),chargeQuantityDomain.getId(),
-								districtDomain.getId(),mobildDataDomain.getId());
+						List<Integer> productKeyAttrValueIds = Arrays.asList(supplierDomain.getId(), chargeQuantityDomain.getId(), districtDomain.getId(),
+								mobildDataDomain.getId());
 						createProductParam.setProductKeyAttrValueIds(productKeyAttrValueIds);
 						createProductParam.setProductMemo("测试");
-						String productName = StringTools.concat("流量充值",supplierDomain.getAttrDisplayValue(),
-								chargeQuantityDomain.getAttrDisplayValue(),districtDomain.getAttrDisplayValue(),
-								mobildDataDomain.getAttrDisplayValue());
+						String productName = StringTools.concat("流量充值", supplierDomain.getAttrDisplayValue(), chargeQuantityDomain.getAttrDisplayValue(),
+								districtDomain.getAttrDisplayValue(), mobildDataDomain.getAttrDisplayValue());
 						createProductParam.setProductName(productName);
-						
+
 						createProductParam.setProductNormalAttrValueIds(null);
-						String thumbnailHead = StringTools.concatWithSymbol("-",supplierDomain.getAttrDisplayEnValue(),
-								chargeQuantityDomain.getAttrDisplayEnValue(),
-								districtDomain.getAttrDisplayEnValue(),
-								mobildDataDomain.getAttrDisplayEnValue());
-						String thumbnail = StringTools.concatWithSymbol(".",thumbnailHead,"jpg");
+						String thumbnailHead = StringTools.concatWithSymbol("-", supplierDomain.getAttrDisplayEnValue(),
+								chargeQuantityDomain.getAttrDisplayEnValue(), districtDomain.getAttrDisplayEnValue(), mobildDataDomain.getAttrDisplayEnValue());
+						String thumbnail = StringTools.concatWithSymbol(".", thumbnailHead, "jpg");
 						createProductParam.setThumbnail(thumbnail);
-						createProductParam.setProductSkuAttrNameIds(null);						
+						createProductParam.setProductSkuAttrNameIds(null);
 						ProductDomain productDomain = productService.createProduct(createProductParam);
-						
+
 						/**
 						 * 产品SKU
 						 */
@@ -365,10 +397,10 @@ public class EnvironmentController {
 						createSkuParam.setThumbnail(productDomain.getThumbnail());
 						createSkuParam.setWeight(0);
 						createSkuParam.setWeightUnit(ConstProduct.WeightUnit.GRAM);
-						createSkuParam.setWidth(0);	
+						createSkuParam.setWidth(0);
 						createSkuParam.setHeight(0);
 						SkuDomain skuDomain = skuService.createSku(createSkuParam);
-						
+
 						/**
 						 * 库存
 						 */
@@ -381,7 +413,7 @@ public class EnvironmentController {
 						createStockParam.setStockQuantity(999999);
 						createStockParam.setWarehouseCode("warehouse1");
 						stockService.createStock(createStockParam);
-						
+
 						/**
 						 * sku上架
 						 */
@@ -390,7 +422,7 @@ public class EnvironmentController {
 						updateSkuStatusParam.setOperatorCode(userCode);
 						updateSkuStatusParam.setSkuStatus(ConstProduct.SkuStatus.ON_SALE);
 						skuService.updateSkuStatus(updateSkuStatusParam);
-						
+
 						/**
 						 * product上架
 						 */
@@ -403,15 +435,13 @@ public class EnvironmentController {
 						updateProductStatusParam.setOperatorCode(userCode);
 						updateProductStatusParam.setProductStatus(ConstProduct.ProductStatus.ON_SALE);
 						productService.updateProductStatus(updateProductStatusParam);
-						
-						logger.info("Succeeded to generate product : {}" ,productDomain.getProductName());
-						
+
+						logger.info("Succeeded to generate product : {}", productDomain.getProductName());
+
 					}
 				}
 			}
 		}
-		
-		logger.info("Succeeded to generate all products" );
 	}
 
 }
