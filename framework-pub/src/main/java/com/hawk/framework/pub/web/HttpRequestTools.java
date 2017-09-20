403eba0e631e929d1019cf60903f9e2abce3bc80
zeatul@qq.com <zeatul@qq.com>
2017-09-20 15:26:50 +0800

    1

--- framework-pub/src/main/java/com/hawk/framework/pub/web/HttpRequestTools.java
+++ framework-pub/src/main/java/com/hawk/framework/pub/web/HttpRequestTools.java
@@ -2,6 +2,8 @@ package com.hawk.framework.pub.web;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,7 +15,7 @@ import com.hawk.framework.utility.tools.JsonTools;
 import com.hawk.framework.utility.tools.StringTools;
 
 public class HttpRequestTools {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(HttpRequestTools.class);
 
 	/**
@@ -39,34 +41,96 @@ public class HttpRequestTools {
 		}
 
 		T t = JsonTools.toObject(input, clazz);
-		
-		logger.info("requestParams={}",JsonTools.toJsonString(t));
+
+		logger.info("requestParams={}", JsonTools.toJsonString(t));
 
 		return t;
 
 	}
 
+	// /**
+	// * 获取请求的客户端ip地址
+	// *
+	// * @param request
+	// * @return
+	// */
+	// public static String getIp(HttpServletRequest request) {
+	// String ip = request.getHeader("x-forwarded-for");
+	//
+	// if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	// ip = request.getHeader("Proxy-Client-IP");
+	// }else{
+	// // 多次反向代理后会有多个ip值，第一个ip才是真实ip
+	// if( ip.indexOf(",")!=-1 ){
+	// ip = ip.split(",")[0];
+	// }
+	// }
+	//
+	// if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	// ip = request.getHeader("WL-Proxy-Client-IP");
+	// }
+	// if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	// ip = request.getRemoteAddr();
+	// }
+	// return ip;
+	// }
+
 	/**
-	 * 获取请求的客户端ip地址
+	 * 获取用户真实IP地址，不使用request.getRemoteAddr()的原因是有可能用户使用了代理软件方式避免真实IP地址,
+	 * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值
 	 * 
-	 * @param request
-	 * @return
+	 * @return ip
 	 */
 	public static String getIp(HttpServletRequest request) {
 		String ip = request.getHeader("x-forwarded-for");
+		logger.debug("x-forwarded-for ip: {}", ip);
+
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
+			logger.debug("Proxy-Client-IP ip: {}", ip);
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
+			logger.debug("WL-Proxy-Client-IP ip: {}", ip);
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+			logger.debug("HTTP_CLIENT_IP ip: " + ip);
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+			logger.debug("HTTP_X_FORWARDED_FOR ip: {}", ip);
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("X-Real-IP");
+			logger.debug("X-Real-IP ip: {}", ip);
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
+			logger.debug("getRemoteAddr ip: {}", ip);
 		}
+		logger.debug("获取客户端ip: {}", ip);
+
+		if (ip != null && (ip.equals("127.0.0.1") || ip.equals("0:0:0:0:0:0:0:1"))) {
+			// 根据网卡获取本机配置的IP地址
+			InetAddress inetAddress = null;
+			try {
+				inetAddress = InetAddress.getLocalHost();
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+			ip = inetAddress.getHostAddress();
+		}
+
+		// 对于通过多个代理的情况，第一个IP为客户端真实的IP地址，多个IP按照','分割
+		if (ip != null && ip.indexOf(",") != -1) {
+			ip = ip.split(",")[0];
+		}
+
 		return ip;
 	}
-	
-	public static String getUserAgent(HttpServletRequest request){
+
+	public static String getUserAgent(HttpServletRequest request) {
 		return request.getHeader("User-Agent");
 	}
 
