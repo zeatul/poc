f6ee6e2b30471950cd20eb27815c9d25ff149c48
zeatul@qq.com <zeatul@qq.com>
2017-09-22 16:02:24 +0800

    1

--- e-commerce/e-commerce-ecom-pay-service/src/main/java/com/hawk/ecom/pay/response/WXPayReturnResponse.java
+++ e-commerce/e-commerce-ecom-pay-service/src/main/java/com/hawk/ecom/pay/response/WXPayReturnResponse.java
@@ -3,5 +3,13 @@ package com.hawk.ecom.pay.response;
 import com.hawk.framework.pub.web.ResponseData;
 
 public class WXPayReturnResponse implements ResponseData {
+	public String getResult() {
+		return result;
+	}
 
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	private String result = "付款可能成功，脑残微信支付说";
 }
